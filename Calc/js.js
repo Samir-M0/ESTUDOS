aab6e5ec5fd8370b0aa903f4a77814712430d045
samir <samirmendes122@gmail.com>
2025-03-12 20:14:40 -0300

    modified:   .gitignore modified:   Calc/js.js

--- Calc/js.js
+++ Calc/js.js
@@ -10,7 +10,7 @@ let ultimoParentesis
 
 function clicar(valor) {
 
-  if (valor.innerText == ',' ) {
+  if (valor.innerText == ',') {
     if (temVirgula == false) {
       display.value += valor.innerText
       temVirgula = true
@@ -22,28 +22,28 @@ function clicar(valor) {
 
 
 
-  if (valor.innerText == '+' || valor.innerText == '-'  || valor.innerText == '/'  || valor.innerText == 'x'  || valor.innerText == '%') {
+  if (valor.innerText == '+' || valor.innerText == '-' || valor.innerText == '/' || valor.innerText == 'x' || valor.innerText == '%') {
     temVirgula = false;
-    
-    if (temOperador) { 
-        return;
+
+    if (temOperador) {
+      return;
     }
-    
+
     display.value += valor.innerText;
     temOperador = true;
 
-} 
-else if(valor.innerText == '(' || valor.innerText ==  ')'){
-  display.value += valor.innerText;
-  temOperador = false; 
+  }
+  else if (valor.innerText == '(' || valor.innerText == ')') {
+    display.value += valor.innerText;
+    temOperador = false;
 
 
 
   }
-else if (/([0-9()])/.test(valor.innerText)) { 
+  else if (/([0-9()])/.test(valor.innerText)) {
     display.value += valor.innerText;
-    temOperador = false; 
-}
+    temOperador = false;
+  }
 
 }
 function apagar() {
@@ -61,7 +61,7 @@ function calculo() {
   temVirgula = false
   // entrada = display,value
   let resultadoTratamento = true
-  arrayEntrada = display.value.replaceAll(',','.').split(/([+\-x/\(\)%])/).filter(token => token.trim() !== "")
+  arrayEntrada = display.value.replaceAll(',', '.').split(/([+\-x/\(\)%])/).filter(token => token.trim() !== "")
   // console.log(arrayEntrada)
   // console.log(display.value)
 
@@ -78,7 +78,7 @@ function calculo() {
       resultadoTratamento = false
       break;
     }
-    else if (arrayEntrada.length == 1){
+    else if (arrayEntrada.length == 1) {
       display.value = Number(arrayEntrada[0])
       return;
     }
@@ -127,7 +127,7 @@ function multiDiv(params) {
       arrayEntrada[i - 1] = Number(arrayEntrada[i - 1]) / 100
       resultadoConta = Number(arrayEntrada[i - 1]) * Number(arrayEntrada[i + 1]);
       // console.log(resultadoConta) 
-    
+
       arrayEntrada.splice(i - 1, i + 2, resultadoConta)
       // console.log(arrayEntrada)
 
@@ -137,13 +137,13 @@ function multiDiv(params) {
         // console.log(arrayEntrada)
       }
     }
-  display.value = resultadoConta.toFixed(2).replaceAll(".", ",")
+    display.value = resultadoConta.toFixed(2).replaceAll(".", ",")
 
-    
+
 
 
   }
-  
+
 }
 function somaSubtracao(params) {
   for (let i = 1; i < arrayEntrada.length; i += 2) {
@@ -187,7 +187,7 @@ function somaSubtracao(params) {
 
   }
   display.value = resultadoConta.toFixed(2).replaceAll(".", ",")
-  
+
 }
 
 function cacador() {
@@ -195,18 +195,22 @@ function cacador() {
   arrayEntrada.forEach((element, index) => {
 
     if (element == "(") {
-      
+
       //  console.log(index)
-       return parentesisDireito.push(index);
-      
+      return parentesisDireito.push(index);
+
     }
     if (element == ")") {
-      
+
       //  console.log(index)
-       return parentesisEsquerdo.push(index);
-      
+      return parentesisEsquerdo.push(index);
+
     }
   });
+}
+function isParentesis(val) {
+  return val !== '(' && val !== ')';
+
 }
 
 function resultado() {
@@ -221,68 +225,124 @@ function resultado() {
     cacador(arrayEntrada)
     console.log(parentesisDireito)
     console.log(parentesisEsquerdo)
-    for (let i = parentesisDireito.length -1; i >=0; i--) {
+    for (let i = parentesisDireito.length - 1; i >= 0; i--) {
       // console.log(parentesisDireito[i])
       // console.log(parentesisEsquerdo[i])
       // console.log(i)
-      arrayPraContaParentesis = arrayEntrada.slice(parentesisDireito[i], parentesisEsquerdo[i+1])
+      arrayPraContaParentesis = arrayEntrada.slice(parentesisDireito[i], parentesisEsquerdo[i + 1])
       // console.log(arrayPraContaParentesis)
-      if (arrayPraContaParentesis[0] == '(') {
-        arrayPraContaParentesis.shift()
-
-        arrayPraContaParentesis.pop()
+      if (arrayPraContaParentesis.includes('(')) {
+        console.log("Ai meu")
+        arrayPraContaParentesis = arrayPraContaParentesis.filter(isParentesis)
         console.log(arrayPraContaParentesis)
 
-      }
-  
+        for (let j = 1; j <= arrayPraContaParentesis.length; j += 2) {
+          operador = arrayPraContaParentesis[j]
 
+          if (operador == 'x') {
+            resultadoConta = Number(arrayPraContaParentesis[j - 1]) * Number(arrayPraContaParentesis[j + 1])
       
+            arrayPraContaParentesis.splice(j - 1, j + 2, resultadoConta)
+            
+            arrayEntrada.splice(5,9, resultadoConta)
+            parentesisDireito.shift()
+            parentesisEsquerdo.shift()
+            console.log(arrayPraContaParentesis)
+            console.log(arrayEntrada)
+
+            if (arrayEntrada.length >= 1) {
+              j = -1
+              // console.log(j)
+              // console.log(arrayEntrada)
+            }
+          }
+          if (operador == '/') {
+            resultadoConta = Number(arrayPraContaParentesis[j - 1]) / Number(arrayPraContaParentesis[j + 1])
       
-    }
-  }}
-  for (let i = 1; i <= arrayEntrada.length; i += 2) {
+            arrayPraContaParentesis.splice(j - 1, j + 2, resultadoConta)
+            console.log(arrayPraContaParentesis)
+            if (arrayEntrada.length >= 1) {
+              j = -1
+              // console.log(j)
+              // console.log(arrayEntrada)
+            }
+          }
+          if (operador == '*') {
+            arrayPraContaParentesis[j - 1] = Number(arrayPraContaParentesis[j - 1]) / 100
 
-
-    operador = arrayEntrada[i]
-    // console.log(operador)
-    if (operador == 'x') {
-      multiDiv()
-
-
-    }
-    if (operador == '/') {
-      multiDiv()
-
-
-
-    }
-
-    if (operador == '%') {
-      multiDiv()
-
-
-    }
-
-
-  }
-
-  for (let i = 1; i < arrayEntrada.length; i += 2) {
-
-    operador = arrayEntrada[i]
-
-
-    if (operador == '+') {
-      somaSubtracao()
-
-      }
-
-    if (operador == '-') {
-      somaSubtracao()
+            resultadoConta = Number(arrayPraContaParentesis[j - 1]) * Number(arrayPraContaParentesis[j + 1])
+      
+            arrayPraContaParentesis.splice(j - 1, j + 2, resultadoConta)
+            console.log(arrayPraContaParentesis)
+            if (arrayEntrada.length >= 1) {
+              j = -1
+              // console.log(j)
+              // console.log(arrayEntrada)
+            }
+          }
 
         }
+
+
+
       }
 
-  // resultadoConta.replace(".", ",")
+
+
+
+    }
+  }
+  else{
+    for (let i = 1; i <= arrayEntrada.length; i += 2) {
+  
+  
+      operador = arrayEntrada[i]
+      // console.log(operador)
+      if (operador == 'x') {
+        multiDiv()
+    
+    
+      }
+      if (operador == '/') {
+        multiDiv()
+    
+    
+    
+      }
+    
+      if (operador == '%') {
+        multiDiv()
+    
+    
+      }
+    
+    
+    }
+    
+    for (let i = 1; i < arrayEntrada.length; i += 2) {
+    
+      operador = arrayEntrada[i]
+    
+    
+      if (operador == '+') {
+        somaSubtracao()
+    
+      }
+    
+      if (operador == '-') {
+        somaSubtracao()
+    
+      }
+    }
+  
+  
+  
+  }
+  
+}
+
+
+// resultadoConta.replace(".", ",")
 
 
 
