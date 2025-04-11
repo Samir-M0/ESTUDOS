153a9570dcfcebe9a00bedce8a4c6dc344068aac
samir <samirmendes122@gmail.com>
2025-04-11 14:42:08 -0300

    Salvando mudanças antes do revert

--- PHP/GuzzleDomCrawler/BuscadorDeLivrosPrecos.php
+++ PHP/GuzzleDomCrawler/BuscadorDeLivrosPrecos.php
@@ -3,9 +3,11 @@ require_once "vendor/autoload.php";
 
 use App\BuscaLivros;
 use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
 
 function buscar(){
   $client = new Client(['base_uri' => 'https://www.estantevirtual.com.br/l']);
+  $crawler = new Crawler();
   $url = "/lst/literatura-russa";
   $buscar = new BuscaLivros($client, $url);
   
